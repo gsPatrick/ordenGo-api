b9d0acfee475be61878542a4ca783e044d61ebc4
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-11-22 15:04:28 -0300

    package.json

--- src/app.js
+++ src/app.js
@@ -132,7 +132,7 @@ async function createDefaultSuperAdmin() {
 // ============================================================
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync({ alter: false }) 
+sequelize.sync({ force: true }) 
   .then(async () => {
     console.log('💾 Banco de dados conectado e sincronizado.');
     
