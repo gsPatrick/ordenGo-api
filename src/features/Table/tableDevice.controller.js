8003f1437f56e17a6c3fec2997fb56310a17a55a
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-11-27 01:35:11 -0300

    models

--- src/features/Table/tableDevice.controller.js
+++ src/features/Table/tableDevice.controller.js
@@ -1,5 +1,5 @@
 const tableDeviceService = require('./tableDevice.service');
-const catchAsync = require('../../../utils/catchAsync');
+const catchAsync = require('../../utils/catchAsync');
 const AppError = require('../../utils/AppError');
 
 // ============================================================
